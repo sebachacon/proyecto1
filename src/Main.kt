32375c2a14e0e3b7a7c3643b771ab74c604cc5e6
sebachacon <ch18082008@gmail.com>
2024-05-21 08:16:50 -0600

    mi primir comit

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    //print sirve para imprimir texto en consola
+    print("hola mundo")
+
+    //Variable de tipo numerico
+
+    val edad: Int // declarar la avariable y su tipo
+    edad = 28
+    val sueldo: Float // declarar numerico con decimales
+    sueldo = 1200.55f
+    val total: Double
+    total = 70000.24
+    val titulo: String // cadena de caracteres
+    titulo = "Sistema de Ventanas"
+
+    var mes: Int
+    mes = 1
+    // quiero cambiar el valor
+    mes = 2
+
+    // Variable de tipo Char, me permite almacenar un solom caracter
+    var continuar: Char = 's'
+
+    //variable tipo boolean true / false
+    var fin: Boolean = false
+}
